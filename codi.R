f54c1ad3d46cb6d599d5282497f574b8af7ea1a9
Ticoloi <130762407+Ticoloi@users.noreply.github.com>
2025-11-29 17:33:12 +0100

    Actualitzat
    Refactor data loading and summarization in codi.R.

--- codi.R
+++ codi.R
@@ -3,14 +3,23 @@ library(tidyverse)
 library(simmer)
 library(simmer.plot)
 
+
 data3<- fread("aigua.csv")
 #save.image(file="aigua.RData")
-
-load("~/Documents/Programacio/R/water/aigua.RData")
+#load("~/Documents/Programacio/R/water/aigua.RData")
 # Save the city object
-#saveRDS(data3, "aigua.rdata"
+#saveRDS(data3, "aigua.rdata")
 
+glimpse(data3)
+summarise(data3,
+          n = n(),
+          n.na = sum(is.na(`Codi comarca`)))
 
 summarise(data3,
-          any = any(),
-          n.na = sum(is.na(resource)))
+          n.cat = n_distinct(`Codi comarca`))
+
+recompte = count(data3, `Codi comarca`)
+arrange(recompte, desc(n))
+
+x = pull(arrange(recompte, desc(n)), n, name = `Codi comarca`)
+barplot(x, las = 2, cex.names = 0.7)
